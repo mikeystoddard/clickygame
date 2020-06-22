a46993827b24bce8e53bada9673cadfd2e7e381e
Michael <mikeystoddard@gmail.com>
2020-06-22 16:36:55 -0700

    Updates

---
+++ precache-manifest.e280fdaf4a6dd1c9bbfd419c76676123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b583aa77fdecd4e80c3a844accf08368",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "071570e0a563472b6a3e",
+    "url": "/clickygame/static/css/main.e6fbfcbb.chunk.css"
+  },
+  {
+    "revision": "9c4d0db947cc959a6c16",
+    "url": "/clickygame/static/js/2.15409e2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.15409e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071570e0a563472b6a3e",
+    "url": "/clickygame/static/js/main.4d12ca66.chunk.js"
+  },
+  {
+    "revision": "7122649522b9616ddb97",
+    "url": "/clickygame/static/js/runtime-main.71d67497.js"
+  }
+]);
